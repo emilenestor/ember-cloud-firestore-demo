7d671d37074d49ddb2bc3f17c19da84fea2d0fa3
SMAB\emne <emile.nestor@wgsa.se>
2021-11-17 15:28:37 +0100

    Added findAll functionality to application route model and html to template

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,3 +1,11 @@
 import Route from '@ember/routing/route';
 
-export default class ApplicationRoute extends Route {}
+export default class ApplicationRoute extends Route {
+    model() {
+        return this.store.findAll('post', {
+            adapterOptions: {
+                isRealtime: true,
+            },
+        });
+    }
+}
