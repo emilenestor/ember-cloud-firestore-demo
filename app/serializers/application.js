41514aaf589c9ff703337742f72ce8753ea01336
SMAB\emne <emile.nestor@wgsa.se>
2021-11-17 15:14:37 +0100

    ember g model post title:string body:string

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,3 +1,3 @@
 import CloudFirestoreSerializer from 'ember-cloud-firestore-adapter/serializers/cloud-firestore-modular';
 
-export default class ApplicationSerializer extends CloudFirestoreSerializer { }
+export default class ApplicationSerializer extends CloudFirestoreSerializer {}
